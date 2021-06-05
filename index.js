69c4a5dcbd318fb130ba3b488acd7204fcda0825
Andyiev <ayevdokymenmk@gmail.com>
2021-06-04 21:55:21 -0600

    LightCoin

--- index.js
+++ index.js
@@ -1,29 +1,70 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+  constructor() {
+    this.transactions = [];
   }
-
-  commit() {
-    balance -= this.amount;
+  get balance() { // Calculate the balance
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;
   }
-
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
+  }
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+  isAllowed() {
+    return true;
+  }
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+}
+
+const myAccount = new Account("snow-patrol");
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(300.00, myAccount);
+console.log('Commit result:', t1.commit());
+console.log('New Balance: ', myAccount.balance);
+
+const t2 = new Withdrawal(45.00, myAccount);
+console.log('Commit result:', t2.commit());
+console.log('New Balance: ', myAccount.balance);
+
+const t3 = new Deposit(75.00, myAccount);
+console.log('Commit result:', t3.commit());
+console.log('New Balance: ', myAccount.balance);
+
+console.log('Account Transactions: ', myAccount.balance);
+console.log('Balance:', myAccount.balance);
 
 
-
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
